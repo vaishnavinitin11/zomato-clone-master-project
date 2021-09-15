ceb97fb8f42756ec032b757fd45539e72434923a
Vaishnavi Uttarkar <vaishnavinitin11@gmail.com>
2021-09-15 19:51:48 +0530

    add route config

--- server/API/orders/index.js
+++ server/API/orders/index.js
@@ -6,7 +6,7 @@ import passport from "passport";
 import { OrderModel } from "../../database/allModels";
 
 // Validations
-import { ValidateOrderId, ValidateNewOrderId  } from "../../validation/order";
+import { ValidateOrderId, ValidateNewOrderId } from "../../validation/order";
 
 const Router = express.Router();
 
@@ -18,21 +18,25 @@ Access Public
 Method GET
 */
 
-Router.get("/", async (req, res) => {
-  try {
-    await ValidateOrderId(req.params);
-    const { _id } = req.params;
-    const getOrders = await OrderModel.findOne({ user: _id });
+Router.get(
+  "/:_id",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    try {
+      await ValidateOrderId(req.params);
+      const { _id } = req.params;
+      const getOrders = await OrderModel.findOne({ user: _id });
 
-    if (!getOrders) {
-      return res.status(404).json({ error: "User Not Found" });
+      if (!getOrders) {
+        return res.status(404).json({ error: "User Not Found" });
+      }
+
+      return res.status(200).json({ orders: getOrders });
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
     }
-
-    return res.status(200).json({ orders: getOrders });
-  } catch (error) {
-    return res.status(500).json({ error: error.message });
   }
-});
+);
 
 /*
 Route  /new
