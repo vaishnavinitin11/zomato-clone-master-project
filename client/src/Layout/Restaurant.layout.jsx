cc753f63ba59d683f52754af182a042fbdaaf084
Vaishnavi Uttarkar <vaishnavinitin11@gmail.com>
2021-09-24 23:55:05 +0530

    add infoButtons

--- client/src/Layout/Restaurant.layout.jsx
+++ client/src/Layout/Restaurant.layout.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { TiStarOutline } from "react-icons/ti";
+import { RiDirectionLine, RiShareForwardLine } from "react-icons/ri";
+import { BiBookmarkPlus } from "react-icons/bi";
 
 // components
 import RestaurantNavbar from "../Components/Navbar/restaurantNavbar";
 import ImageGrid from "../Components/Restaurant/imageGrid";
+import InfoButtons from "../Components/Restaurant/InfoButtons";
+import Restaurantinfo from "../Components/Restaurant/Restaurantinfo";
 
 const RestaurantLayout = () => {
   return (
@@ -18,6 +23,27 @@ const RestaurantLayout = () => {
             "https://b.zmtcdn.com/data/pictures/chains/2/19693512/6a660f14340efbb18e58c104dcfd6f7d.jpg",
           ]}
         />
+        <Restaurantinfo
+          name="Focaccia Fellas"
+          restaurantRating="3.5"
+          deliveryRating="4.3"
+          cuisine="Fast Food, Sandwich"
+          address="Marol, Mumbai"
+        />
+        <div className="my-4 flex flex-wrap gap-2">
+          <InfoButtons isActive>
+            <TiStarOutline /> Add Review
+          </InfoButtons>
+          <InfoButtons>
+            <RiDirectionLine /> Direction
+          </InfoButtons>
+          <InfoButtons>
+            <BiBookmarkPlus /> Bookmark
+          </InfoButtons>
+          <InfoButtons>
+            <RiShareForwardLine /> Share
+          </InfoButtons>
+        </div>
       </div>
     </>
   );
