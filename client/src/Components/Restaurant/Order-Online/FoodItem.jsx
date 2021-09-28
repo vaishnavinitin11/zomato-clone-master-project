79757dde8afe75aea219454b5f26f5bd70ea2040
Vaishnavi Uttarkar <vaishnavinitin11@gmail.com>
2021-09-29 00:48:02 +0530

    add food list

--- client/src/Components/Restaurant/Order-Online/FoodItem.jsx
+++ client/src/Components/Restaurant/Order-Online/FoodItem.jsx
@@ -5,7 +5,7 @@ import { AiOutlinePlus } from "react-icons/ai";
 const FoodItem = (props) => {
   return (
     <>
-      <div className="flex items-start gap-2 ">
+      <div className="flex items-start gap-2">
         <div className="w-3/12 h-24 md:h-28 lg:h-36 md:px-3">
           <img
             src={props.image}
@@ -13,7 +13,7 @@ const FoodItem = (props) => {
             className=" w-full h-full rounded-lg"
           />
         </div>
-        <div className="w-3/4 md:w-7/12  ">
+        <div className="w-3/4 md:w-7/12">
           <div className="flex items-center justify-between">
             <h3 className="text-xl  font-semibold">{props.title}</h3>
             <button className=" md:hidden flex items-center gap-2 text-zomato-400 bg-zomato-50 border border-zomato-400 px-2 py-1 rounded-lg">
@@ -25,7 +25,7 @@ const FoodItem = (props) => {
           <h5>₹{props.price}</h5>
           <p className="truncate">{props.description}</p>
         </div>
-        <div className="hidden md:block w-2/12 ">
+        <div className="hidden md:block w-2/12">
           <button className="flex items-center gap-2 text-zomato-400 bg-zomato-50 border border-zomato-400 px-4 py-2 rounded-lg">
             <AiOutlinePlus />
             Add
--- client/src/Page/Restaurant/OrderOnline.jsx
+++ client/src/Page/Restaurant/OrderOnline.jsx
@@ -5,31 +5,51 @@ import { BiTimeFive } from "react-icons/bi";
 // components
 import FloatMenuBtn from "../../Components/Restaurant/Order-Online/FloatMenuBtn";
 import FoodItem from "../../Components/Restaurant/Order-Online/FoodItem";
+import FoodList from "../../Components/Restaurant/Order-Online/FoodList";
 import MenuListContainer from "../../Components/Restaurant/Order-Online/MenuListContainer";
+
 
 const OrderOnline = () => {
   return (
     <>
-      <div className="w-full flex">
-        <aside className="hidden md:flex flex-col border-r overflow-y-scroll border-gray-200 h-screen gap-3 w-1/4">
+      <div className="w-full h-screen flex">
+        <aside className="hidden md:flex flex-col gap-3 border-r overflow-y-scroll border-gray-200 h-screen w-1/4">
           <MenuListContainer />
           <MenuListContainer />
           <MenuListContainer />
           <MenuListContainer />
           <MenuListContainer />
         </aside>
-        <div className="w-full md:w-3/4">
-          <h2 className="text-xl font-medium ">Order Online</h2>
-          <h4 className="flex items-center gap-2 font-light text-gray-500">
-            <AiOutlineCompass /> Live Track Your Order | <BiTimeFive /> 45 min
-          </h4>
-          <section>
-            <FoodItem
-              image="https://b.zmtcdn.com/data/dish_photos/798/7c23183977751ce9596b2c9e8e237798.jpg"
-              price="1000"
-              rating={3}
-              description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quod blanditiis dolorem possimus explicabo non sapiente totam ducimus. Quidem soluta non quos quae odio reprehenderit magni! Consequuntur fugit sit dolor."
-              title="Eggplant & Potato Moussaka Marinara"
+        <div className="w-full px-3 md:w-3/4">
+          <div className="pl-3 mb-4 ">
+            <h2 className="text-xl font-medium ">Order Online</h2>
+            <h4 className="flex items-center gap-2 font-light text-gray-500">
+              <AiOutlineCompass /> Live Track Your Order | <BiTimeFive /> 45 min
+            </h4>
+          </div>
+          <section className="flex h-screen overflow-y-scroll flex-col gap-3 md:gap-5">
+            <FoodList
+              title="Recommended"
+              items={[
+                {
+                  image:
+                    "https://b.zmtcdn.com/data/dish_photos/798/7c23183977751ce9596b2c9e8e237798.jpg",
+                  price: "1000",
+                  rating: 3,
+                  description:
+                    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quod blanditiis dolorem possimus explicabo non sapiente totam ducimus. Quidem soluta non quos quae odio reprehenderit magni! Consequuntur fugit sit dolor.",
+                  title: "Eggplant & Potato Moussaka Marinara",
+                },
+                {
+                  image:
+                    "https://b.zmtcdn.com/data/dish_photos/798/7c23183977751ce9596b2c9e8e237798.jpg",
+                  price: "1000",
+                  rating: 3,
+                  description:
+                    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quod blanditiis dolorem possimus explicabo non sapiente totam ducimus. Quidem soluta non quos quae odio reprehenderit magni! Consequuntur fugit sit dolor.",
+                  title: "Eggplant & Potato Moussaka Marinara",
+                },
+              ]}
             />
           </section>
         </div>
