635f84b45bd53b226c51cc0e96a82aa6f40e1486
Vaishnavi Uttarkar <vaishnavinitin11@gmail.com>
2021-10-04 14:09:47 +0530

    add redux

--- client/src/Redux/store.js
+++ client/src/Redux/store.js
@@ -12,6 +12,6 @@ if (process.env.NODE_ENV === "development") {
   middlewares.push(logger);
 }
 
-const store = createStore(rootReducer, {}, applyMiddleware(...middlewares));
+const store = createStore(rootReducer, {}, applyMiddleware(...middlewares ));
 
 export default store;
